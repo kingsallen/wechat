ed1e40d0bbf4ca50a016843ca611d0270d8cb778
tangyiliang <tangyiliang@moseeker.com>
2016-10-19 13:00:39 +0800

    fix: landing will not use render_json

--- handler/base.py
+++ handler/base.py
@@ -404,8 +404,7 @@ class BaseHandler(MetaBaseHandler):
         self.set_secure_cookie(self.constant.COOKIE_SESSIONID, session_id)
         self.redis.set(
             self.constant.SESSION_USER.format(session_id, self._wechat.id),
-            ujson.dumps(session),
-            60 * 60 * 2)
+            session, 60 * 60 * 2)
 
         if self.is_platform:
             employee = yield self.session_ps.get_employee(
@@ -517,28 +516,32 @@ class BaseHandler(MetaBaseHandler):
         #                 message="正在努力维护服务器中")
         self.write(http_code)
 
-    def render(self, template_name, data, status_code=0, message='success', http_code=200):
+    def render(self, template_name, data, status_code=0, message='success', http_code=200, use_render_json=True):
         """render 页面"""
         self.log_info = {"res_type": "html", "status_code": status_code}
         self.set_status(http_code)
 
-        render_json = encode_json_dumps({
-            "status": status_code,
-            "message": message,
-            "data": data
-        })
+        if use_render_json:
+            render_json = encode_json_dumps({
+                "status": status_code,
+                "message": message,
+                "data": data
+            })
 
-        # 前后端联调使用
-        if self.settings.get('remote_debug', False) is True:
-            template_string = self.render_string(template_name, render_json)
-            post_url = urljoin(self.settings.get('remote_debug_ip'),
-                               template_name)
-            http_client = tornado.httpclient.HTTPClient()
-            r = http_client.fetch(post_url, method="POST",
-                                  body=template_string)
-            self.write(r.body)
-            self.finish()
-            return
+            # 前后端联调使用
+            if self.settings.get('remote_debug', False) is True:
+                template_string = self.render_string(template_name, render_json)
+                post_url = urljoin(self.settings.get('remote_debug_ip'),
+                                   template_name)
+                http_client = tornado.httpclient.HTTPClient()
+                r = http_client.fetch(post_url, method="POST",
+                                      body=template_string)
+                self.write(r.body)
+                self.finish()
+                return
+
+        else:
+            render_json = data
 
         self.render(template_name, render_json)
         return
