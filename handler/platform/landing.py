3626f42eb346210dd76ddd588d7199afb26e19e6
tangyiliang <tangyiliang@moseeker.com>
2016-10-24 14:16:55 +0800

    fix: make company search page work

--- handler/platform/landing.py
+++ handler/platform/landing.py
@@ -47,5 +47,5 @@ class LandingHandler(BaseHandler):
             "search_seq": search_seq
         })
 
-        self.render_page("refer/neo_weixin/position/company_search.html",
-                         data=company)
+        self.render("refer/neo_weixin/position/company_search.html",
+                    company=company)
