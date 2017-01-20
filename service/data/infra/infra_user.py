9f8a806d1e2c0ec0ff65375e434ffb3992bf89fb
Panda <panyuxin@moseeker.com>
2017-01-20 18:03:20 +0800

    注释 infradataservice

--- service/data/infra/infra_user.py
+++ service/data/infra/infra_user.py
@@ -1,24 +1,24 @@
-# coding=utf-8
-
-from service.data.base import DataService
-import tornado.gen as gen
-from util.common import ObjectDict
-from conf.path import USER_BIND_WX_MOBILE
-from util.tool.http_tool import http_get, http_post, http_put, http_delete, http_patch
-
-
-class InfraUserDataService(DataService):
-
-    @gen.coroutine
-    def post_user_wxbindmobile(self, **kwargs):
-
-        params = ObjectDict({
-            "unionid": kwargs["unionid"],
-            "mobile": kwargs["mobile"],
-            "code": kwargs.get("code" "")
-        })
-
-        ret = yield http_post(USER_BIND_WX_MOBILE, params)
-
-        raise gen.Return(ret)
-
+# # coding=utf-8
+#
+# from service.data.base import DataService
+# import tornado.gen as gen
+# from util.common import ObjectDict
+# from conf.path import USER_BIND_WX_MOBILE
+# from util.tool.http_tool import http_get, http_post, http_put, http_delete, http_patch
+#
+#
+# class InfraUserDataService(DataService):
+#
+#     @gen.coroutine
+#     def post_user_wxbindmobile(self, **kwargs):
+#
+#         params = ObjectDict({
+#             "unionid": kwargs["unionid"],
+#             "mobile": kwargs["mobile"],
+#             "code": kwargs.get("code" "")
+#         })
+#
+#         ret = yield http_post(USER_BIND_WX_MOBILE, params)
+#
+#         raise gen.Return(ret)
+# 
