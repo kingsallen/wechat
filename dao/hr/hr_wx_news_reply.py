50f884f068364fadb3e694126a7ebdc85896af5d
Panda <panyuxin@moseeker.com>
2017-02-09 16:32:17 +0800

    增加微信交互逻辑

--- dao/hr/hr_wx_news_reply.py
+++ dao/hr/hr_wx_news_reply.py
@@ -2,7 +2,7 @@
 
 # @Time    : 10/27/16 12:13
 # @Author  : panda (panyuxin@moseeker.com)
-# @File    : hr_wx_basic_reply.py
+# @File    : hr_wx_news_reply.py
 # @DES     : 微信图文回复表
 
 # Copyright 2016 MoSeeker
@@ -10,14 +10,14 @@
 from dao.base import BaseDao
 
 
-class HrWxBasicReplyDao(BaseDao):
+class HrWxNewsReplyDao(BaseDao):
 
     def __init__(self):
-        super(HrWxBasicReplyDao, self).__init__()
-        self.table = "hr_wx_basic_reply"
+        super(HrWxNewsReplyDao, self).__init__()
+        self.table = "hr_wx_news_reply"
         self.fields_map = {
             "id":              self.constant.TYPE_INT,
-            "rid":             self.constant.TYPE_INT,
+            "rid":             self.constant.TYPE_INT,  # hr_wx_rule.id
             "parentid":        self.constant.TYPE_INT,
             "title":           self.constant.TYPE_STRING,
             "description":     self.constant.TYPE_STRING,
