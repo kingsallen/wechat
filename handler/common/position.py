117fb247b36e09b7badf5cdb7e2c50a43c460eba
tangyiliang <tangyiliang@moseeker.com>
2017-02-24 11:17:03 +0800

    Move starHandler to common/position

---
+++ handler/common/position.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+from tornado import gen
+
+from handler.base import BaseHandler
+from util.common.decorator import handle_response, authenticated
+
+
+class PositionStarHandler(BaseHandler):
+    """处理收藏（加星）操作"""
+
+    @handle_response
+    @authenticated
+    @gen.coroutine
+    def post(self):
+        try:
+            self.guarantee('star', 'pid')
+        except:
+            raise gen.Return()
+
+        # 收藏操作
+        if self.params.star:
+            ret = yield self.user_ps.favorite_position(
+                self.current_user, self.params.pid)
+        else:
+            ret = yield self.user_ps.unfavorite_position(
+                self.current_user, self.params.pid)
+
+        if ret:
+            self.send_json_success()
+        else:
+            self.send_json_error()
--- handler/platform/position.py
+++ handler/platform/position.py
@@ -635,32 +635,6 @@ class PositionHandler(BaseHandler):
         raise gen.Return(res)
 
 
-class PositionStarHandler(BaseHandler):
-    """处理收藏（加星）操作"""
-
-    @handle_response
-    @authenticated
-    @gen.coroutine
-    def post(self):
-        try:
-            self.guarantee('star', 'pid')
-        except:
-            raise gen.Return()
-
-        # 收藏操作
-        if self.params.star:
-            ret = yield self.user_ps.favorite_position(
-                self.current_user, self.params.pid)
-        else:
-            ret = yield self.user_ps.unfavorite_position(
-                self.current_user, self.params.pid)
-
-        if ret:
-            self.send_json_success()
-        else:
-            self.send_json_error()
-
-
 class PositionListHandler(BaseHandler):
     """职位列表"""
 
