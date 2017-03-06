9df044e197915426ea6bf670b0729b516a89a4f8
tangyiliang <tangyiliang@moseeker.com>
2017-03-06 17:13:44 +0800

    Refine dict data service

--- service/data/dict/dict.py
+++ service/data/dict/dict.py
@@ -1,19 +1,18 @@
 # coding=utf-8
 
 
-import itertools
 import collections
+import itertools
 
 import pypinyin
-
-from tornado.util import ObjectDict
 import tornado.gen as gen
-import conf.path as path
-from util.tool.http_tool import http_get
-from util.tool.dict_tool import sub_dict
-from util.common.decorator import cache
+from tornado.util import ObjectDict
 
+import conf.path as path
 from service.data.base import DataService
+from util.common.decorator import cache
+from util.tool.dict_tool import sub_dict
+from util.tool.http_tool import http_get
 
 
 class DictDataService(DataService):
@@ -62,27 +61,29 @@ class DictDataService(DataService):
     # 港澳台城市编号前缀
     _GAT_PREFIX = [81, 82, 71]
 
-    @cache(ttl= 60 * 60 * 5)
+    @cache(ttl=60 * 60 * 5)
     @gen.coroutine
     def get_const_dict(self, parent_code):
         """根据 parent_code 获取常量字典
         :param parent_code: 常量的 parent_code
         """
-
         # 参数校验
         if (parent_code is None or
                 parent_code not in self.CONSTANT_TYPES.values()):
             raise ValueError('invalid parent_code')
 
         params = ObjectDict(parent_code=parent_code)
+        response = yield http_get(path.DICT_CONSTANT, params)
+        return self.make_const_dict_result(response, parent_code)
 
-        res = (yield http_get(path.DICT_CONSTANT, params)).data
-
+    @staticmethod
+    def make_const_dict_result(http_response, parent_code):
+        """获取常量字典后的结果处理"""
+        res_data = http_response.data
         ret = ObjectDict()
-        for el in res.get(str(parent_code)):
+        for el in res_data.get(str(parent_code)):
             el = ObjectDict(el)
             setattr(ret, str(el.code), el.name)
-
         ret = collections.OrderedDict(sorted(ret.items()))
         return ret
 
@@ -90,37 +91,37 @@ class DictDataService(DataService):
     @gen.coroutine
     def get_colleges(self):
         """获取学校列表"""
+        response = yield http_get(path.DICT_COLLEGE, {})
+        return self.make_college_list_result(response)
 
+    @staticmethod
+    def make_college_list_result(http_response):
+        res_data = http_response.data
         collegecode_list = []
-        res = (yield http_get(path.DICT_COLLEGE, {})).data
-
-        if isinstance(res, list):
+        if isinstance(res_data, list):
             collegecode_nested_list = list(itertools.chain(
-                map(lambda x: x.get("children", []), res)))
+                map(lambda x: x.get("children", []), res_data)))
             for cn_list in collegecode_nested_list:
                 for pair in cn_list:
                     collegecode_list.append(pair)
-
         return collegecode_list
 
     @cache(ttl=60 * 60 * 5)
     @gen.coroutine
     def get_college_code_by_name(self, school_name):
         """根据学校名称返回学校 code"""
-
         if school_name is None or not isinstance(school_name, str):
             raise ValueError('invalid school_name')
-
         colleges = yield self.get_colleges()
+        return self.get_code_by_name_from(colleges, school_name)
 
-        ret = list(filter(lambda x: x.get('name') == school_name,
-                          colleges))
-
+    @staticmethod
+    def get_code_by_name_from(colleges, school_name):
+        ret = list(filter(lambda x: x.get('name') == school_name, colleges))
         if ret:
             code = ret[0].get('code', 0)
         else:
             code = 0
-
         return code
 
     @cache(ttl=60 * 60 * 5)
@@ -128,32 +129,43 @@ class DictDataService(DataService):
     def _get_level_1_cities(self):
         """获取一级城市列表"""
         params = dict(level=1, is_using=1)
-        res = (yield http_get(path.DICT_CITIES, params)).data
+        response = yield http_get(path.DICT_CITIES, params)
+        return self._make_level_1_cities_result(response)
+
+    def _make_level_1_cities_result(self, http_response):
+        res_data = http_response.data
         return list(
-            filter(lambda x: x.get('code') in self._LEVEL1_CITY_CODES, res))
+            filter(lambda x: x.get('code') in self._LEVEL1_CITY_CODES,
+                   res_data))
 
     @cache(ttl=60 * 60 * 5)
     @gen.coroutine
     def _get_level_2_cities(self):
         """获取二级城市列表"""
         params = dict(level=2, is_using=1)
-        res = (yield http_get(path.DICT_CITIES, params)).data
-        return res
+        response = yield http_get(path.DICT_CITIES, params)
+        return self._make_level_2_cities_result(response)
+
+    @staticmethod
+    def _make_level_2_cities_result(http_response):
+        return http_response.data
 
     @cache(ttl=60 * 60 * 5)
     @gen.coroutine
     def get_cities(self, hot=False):
         """获取城市列表
-        If hot is True the function only returns hot cities
-        cities are grouped by first pinyin letter
+        hot 为 True 为查询热门城市
         """
+        level1_cities = yield self._get_level_1_cities()
+        level2_cities = yield self._get_level_2_cities()
+        return self.make_cities_result(level1_cities, level2_cities, hot)
 
-        cities1 = yield self._get_level_1_cities()
-        cities2 = yield self._get_level_2_cities()
+    def make_cities_result(self, level1_cities, level2_cities, hot=False):
+        """Merge level1/2 的城市数据，拼装并返回
+        hot 为 True 为查询热门城市
+        """
+        res = level1_cities + level2_cities
 
-        res = cities1 + cities2
-
-        # merge
         cities, ret, heads = [], [], []
         for e in res:
             ret.append(sub_dict(e, ['code', 'name']))
@@ -192,23 +204,26 @@ class DictDataService(DataService):
     @cache(ttl=60 * 60 * 5)
     @gen.coroutine
     def get_hot_cities(self):
-        """alias for get_cities(hot=False) :)"""
+        """Alias for get_cities(hot=False) :)"""
         cities = yield self.get_cities(hot=True)
         return cities
 
     @cache(ttl=60 * 60 * 5)
     @gen.coroutine
     def get_city_code_by(self, city_name):
-        """city city_name -> code
+        """city_name -> code
         :param city_name: city name, type: str
         :return: city code or 0 if city_name is not found
         """
         if city_name is None or not isinstance(city_name, str):
             raise ValueError('invalid city_name')
 
-        res = yield http_get(path.DICT_CITIES, dict(level=2))
+        http_response = yield self._get_level_2_cities()
+        return self.make_city_code_by_name_result(http_response, city_name)
 
-        filtered = list(filter(lambda x: x.get('name') == city_name, res))
+    @staticmethod
+    def make_city_code_by_name_result(response, city_name):
+        filtered = list(filter(lambda x: x.get('name') == city_name, response))
         if filtered:
             return filtered[0].get('code')
         else:
@@ -228,9 +243,12 @@ class DictDataService(DataService):
         except ValueError:
             raise ValueError('invalid city_code')
 
-        res = yield http_get(path.DICT_CITIES, dict(level=2))
+        http_response = yield self._get_level_2_cities()
+        return self.make_city_name_by_code_result(http_response, city_code)
 
-        filtered = list(filter(lambda x: x.get('code') == city_code, res))
+    @staticmethod
+    def make_city_name_by_code_result(response, city_code):
+        filtered = list(filter(lambda x: x.get('code') == city_code, response))
         if filtered:
             return filtered[0].get('name')
         else:
@@ -243,52 +261,51 @@ class DictDataService(DataService):
         industries
         level1 + level2
         """
+        response = yield http_get(path.DICT_INDUSTRY, dict(parent=0))
+        return self.make_industries_result(response)
 
-        level1 = (yield http_get(path.DICT_INDUSTRY, dict(parent=0))).data
+    @staticmethod
+    @gen.coroutine
+    def make_industries_result(http_response):
+        res_data = http_response.data
         industries = []
-
-        for el in level1:
+        for el in res_data:
             el = ObjectDict(el)
             out = ObjectDict()
             out.text = el.name
-            level2 = (
-                yield http_get(path.DICT_INDUSTRY, dict(parent=el.code))).data
+            level2 = yield http_get(path.DICT_INDUSTRY, dict(parent=el.code)).data
             out.list = list(map(lambda x: sub_dict(x, ['code', 'name']), level2))
             industries.append(out)
-
         return industries
 
-    @cache(ttl=60 * 60 * 5)
+    @cache(ttl=60*60*5)
     @gen.coroutine
-    def get_function(self, code=0):
+    def get_functions(self, code=0):
         """获取职能
         Get functions (code=0 -> level1+level2, code!=0 -> level2+level3)
         """
+        if not isinstance(code, int):
+            raise ValueError('invalid code')
 
-        params = dict(parent=code)
+        http_response = yield http_get(path.DICT_POSITION, {})
+        return self.get_function_result(http_response, code)
 
-        @cache(ttl= 60 * 60 * 5)
-        @gen.coroutine
-        def _get_funcs():
-            bunch = (yield http_get(path.DICT_POSITION, params)).data
-            ret = []
-            for b in bunch:
-                b = ObjectDict(b)
-                if code:
-                    o = ObjectDict(sub_dict(b, ['code', 'name']))
-                else:
-                    o = ObjectDict()
-                    o.text = b.name
-                    params.update(parent=b.get('code'))
+    def get_function_result(self, response, code=0):
+        res_data = response.data
+        if not code:
+            # level1 and level2
+            return self.get_function_result_level12(res_data)
+        else:
+            return self.get_function_result_level23(res_data, code)
 
-                    level2 = (yield http_get(path.DICT_POSITION, params)).data
-                    o.list = list(map(
-                        lambda x: sub_dict(x, ['code', 'name']), level2))
+    @staticmethod
+    def get_function_result_level12(res_data):
+        filtered = [f for f in res_data if f['level'] in [1, 2]]
+        return filtered
 
-                ret.append(o)
-            return ret
-
-        # 这么写是因为原先需要缓存部分返回的信息。
-        # 现在不用了，直接调用内部函数
-        functions = yield _get_funcs()
-        return functions
+    @staticmethod
+    def get_function_result_level23(res_data, code):
+        return list(itertools.chain(
+            [f for f in res_data if f['level'] == 2 and f['code'] == code],
+            [f for f in res_data if f['level'] == 3 and f['parent'] == code]
+        ))
