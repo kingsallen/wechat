6975fca02ede7d88ece73e0169f6a0398d74ef81
Panda <panyuxin@moseeker.com>
2017-03-13 12:27:45 +0800

    调整application_ps

--- service/page/application/application.py
+++ service/page/application/application.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+
 import uuid
 import json
 
@@ -15,77 +16,77 @@ from util.wechat.template import application_notice_to_applier_tpl, application_
 
 class ApplicationPageService(PageService):
     pass
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.email_apply_session = EmailApplyCache()
-#
-#     @gen.coroutine
-#     def get_application(self, position_id, user_id):
-#         """返回用户申请的职位"""
-#
-#         if user_id is None:
-#             raise gen.Return(ObjectDict())
-#
-#         ret = yield self.job_application_ds.get_job_application(conds={
-#             "position_id": position_id,
-#             "applier_id": user_id
-#         })
-#         raise gen.Return(ret)
-#
-#     @gen.coroutine
-#     def get_position_applied_cnt(self, conds, fields):
-#         """返回申请数统计"""
-#
-#         response = yield self.job_application_ds.get_position_applied_cnt(conds=conds, fields=fields)
-#
-#         raise gen.Return(response)
-#
-#     @gen.coroutine
-#     def is_allowed_apply_position(self, user_id, company_id):
-#         """获取一个月内该用户的申请数量
-#         返回该用户是否可申请该职位
-#         reference: https://wiki.moseeker.com/application-api.md
-#         :param user_id: 求职者 id
-#         :param company_id: 公司 id
-#         :return:
-#         {
-#           'message': '提示信息',
-#           'status': 0,
-#           'data': true/false   # ture 表示命中限制，不能投递，false 表示可以投递
-#         }
-#
-#         """
-#
-#         if user_id is None or company_id is None:
-#             raise gen.Return(True)
-#
-#         req = ObjectDict({
-#             'user_id': user_id,
-#             'company_id': company_id,
-#         })
-#
-#         ret = yield self.infra_application_ds.get_application_apply_count(req)
-#         bool_res = ret.data if ret.status == 0 else True
-#
-#         raise gen.Return(bool_res)
-#
-#     @gen.coroutine
-#     def update_candidate_company(self, name, user_id):
-#         """
-#         更新candidate_company表中的name
-#         :param user_id:
-#         :return:
-#         """
-#
-#         res = yield self.candidate_company_ds.update_candidate_company(
-#             fields={
-#                 "sys_user_id": user_id
-#             }, conds={
-#                 "name": name,
-#             })
-#         raise gen.Return(res)
-#
+
+    def __init__(self):
+        super().__init__()
+        self.email_apply_session = EmailApplyCache()
+
+    @gen.coroutine
+    def get_application(self, position_id, user_id):
+        """返回用户申请的职位"""
+
+        if user_id is None:
+            raise gen.Return(ObjectDict())
+
+        ret = yield self.job_application_ds.get_job_application(conds={
+            "position_id": position_id,
+            "applier_id": user_id
+        })
+        raise gen.Return(ret)
+
+    @gen.coroutine
+    def get_position_applied_cnt(self, conds, fields):
+        """返回申请数统计"""
+
+        response = yield self.job_application_ds.get_position_applied_cnt(conds=conds, fields=fields)
+
+        raise gen.Return(response)
+
+    @gen.coroutine
+    def is_allowed_apply_position(self, user_id, company_id):
+        """获取一个月内该用户的申请数量
+        返回该用户是否可申请该职位
+        reference: https://wiki.moseeker.com/application-api.md
+        :param user_id: 求职者 id
+        :param company_id: 公司 id
+        :return:
+        {
+          'message': '提示信息',
+          'status': 0,
+          'data': true/false   # ture 表示命中限制，不能投递，false 表示可以投递
+        }
+
+        """
+
+        if user_id is None or company_id is None:
+            raise gen.Return(True)
+
+        req = ObjectDict({
+            'user_id': user_id,
+            'company_id': company_id,
+        })
+
+        ret = yield self.infra_application_ds.get_application_apply_count(req)
+        bool_res = ret.data if ret.status == 0 else True
+
+        raise gen.Return(bool_res)
+
+    @gen.coroutine
+    def update_candidate_company(self, name, user_id):
+        """
+        更新candidate_company表中的name
+        :param user_id:
+        :return:
+        """
+
+        res = yield self.candidate_company_ds.update_candidate_company(
+            fields={
+                "sys_user_id": user_id
+            }, conds={
+                "name": name,
+            })
+        raise gen.Return(res)
+
 #     @gen.coroutine
 #     def custom_check_failure_redirection(self, profile, position, user):
 #         """
