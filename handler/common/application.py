5e10754c58289e057e36dedec734909cad70fa86
tangyiliang <tangyiliang@moseeker.com>
2017-03-17 15:22:30 +0800

    refine application

--- handler/common/application.py
+++ handler/common/application.py
@@ -27,10 +27,14 @@ class ApplicationHandler(BaseHandler):
         position = yield self.position_ps.get_position(self.params.pid)
         check_status, message = yield self.application_ps.check_position(
             position, self.current_user)
-        self.logger.debug( "[create_email_reply]check_status:{}, message:{}".format(check_status, message))
+        self.logger.debug("[create_reply]check_status:{}, message:{}".format(check_status, message))
+
         if not check_status:
-            self.render(template_name='weixin/systemmessage/successapply.html',
-                        message=message, nexturl=make_url(path.POSITION_LIST, self.params, escape=['next_url', 'pid']))
+            self.render(
+                template_name='weixin/systemmessage/successapply.html',
+                message=message,
+                nexturl=make_url(path.POSITION_LIST, self.params,
+                                 escape=['next_url', 'pid']))
             return
 
 
