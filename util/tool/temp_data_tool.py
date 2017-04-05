2b99fa5f2cea0bf1483ed72b4244e352d6d80782
chendi <joker_chendi@126.com>
2017-04-05 12:20:01 +0800

    Figure模块，加上title

--- util/tool/temp_data_tool.py
+++ util/tool/temp_data_tool.py
@@ -96,6 +96,7 @@ def template2(title, data):
 
 def template2_data(resource_list):
     return [{
+                'title': resource.title,
                 'sub_title': resource.sub_title,
                 'description': resource.longtext,
                 'media_url': make_static_url(resource.media_url),
