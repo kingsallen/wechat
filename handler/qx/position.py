d23c406aa06d0395d76978999dd5a9ee700534d7
Panda <panyuxin@moseeker.com>
2017-04-13 14:23:00 +0800

    调整 gamma0.9

---
+++ handler/qx/position.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+# @Time    : 4/13/17 11:42
+# @Author  : panda (panyuxin@moseeker.com)
+# @File    : position.py
+# @DES     : 聚合号职位详情页
+
+from tornado import gen
+from handler.base import BaseHandler
+
+class PositionHandler(BaseHandler):
+
+    @gen.coroutine
+    def get(self):
+
+
+
+
---
+++ handler/qx/positionlist.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+
+# @Time    : 4/13/17 11:43
+# @Author  : panda (panyuxin@moseeker.com)
+# @File    : positionlist.py
+# @DES     :
