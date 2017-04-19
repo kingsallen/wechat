0d96b192e72bdfff4ebf620e2f0bd358d8d76d95
tangyiliang <tangyiliang@moseeker.com>
2017-04-19 13:36:56 +0800

    comment invalid thrift gen code out

--- thrift_gen/gen/dao/struct/dao/ttypes.py
+++ thrift_gen/gen/dao/struct/dao/ttypes.py
@@ -37,97 +37,97 @@ class ConditionType(object):
     }
 
 
-class Condition(object):
-    """
-    Attributes:
-     - conditionType
-     - condition
-     - params
-    """
-
-    thrift_spec = (
-        None,  # 0
-        (1, TType.I32, 'conditionType', None, None, ),  # 1
-        (2, TType.STRUCT, 'condition', (Condition, Condition.thrift_spec), None, ),  # 2
-        (3, TType.LIST, 'params', (TType.STRING, 'UTF8', False), None, ),  # 3
-    )
-
-    def __init__(self, conditionType=None, condition=None, params=None,):
-        self.conditionType = conditionType
-        self.condition = condition
-        self.params = params
-
-    def read(self, iprot):
-        if iprot._fast_decode is not None and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None:
-            iprot._fast_decode(self, iprot, (self.__class__, self.thrift_spec))
-            return
-        iprot.readStructBegin()
-        while True:
-            (fname, ftype, fid) = iprot.readFieldBegin()
-            if ftype == TType.STOP:
-                break
-            if fid == 1:
-                if ftype == TType.I32:
-                    self.conditionType = iprot.readI32()
-                else:
-                    iprot.skip(ftype)
-            elif fid == 2:
-                if ftype == TType.STRUCT:
-                    self.condition = Condition()
-                    self.condition.read(iprot)
-                else:
-                    iprot.skip(ftype)
-            elif fid == 3:
-                if ftype == TType.LIST:
-                    self.params = []
-                    (_etype3, _size0) = iprot.readListBegin()
-                    for _i4 in range(_size0):
-                        _elem5 = iprot.readString().decode('utf-8') if sys.version_info[0] == 2 else iprot.readString()
-                        self.params.append(_elem5)
-                    iprot.readListEnd()
-                else:
-                    iprot.skip(ftype)
-            else:
-                iprot.skip(ftype)
-            iprot.readFieldEnd()
-        iprot.readStructEnd()
-
-    def write(self, oprot):
-        if oprot._fast_encode is not None and self.thrift_spec is not None:
-            oprot.trans.write(oprot._fast_encode(self, (self.__class__, self.thrift_spec)))
-            return
-        oprot.writeStructBegin('Condition')
-        if self.conditionType is not None:
-            oprot.writeFieldBegin('conditionType', TType.I32, 1)
-            oprot.writeI32(self.conditionType)
-            oprot.writeFieldEnd()
-        if self.condition is not None:
-            oprot.writeFieldBegin('condition', TType.STRUCT, 2)
-            self.condition.write(oprot)
-            oprot.writeFieldEnd()
-        if self.params is not None:
-            oprot.writeFieldBegin('params', TType.LIST, 3)
-            oprot.writeListBegin(TType.STRING, len(self.params))
-            for iter6 in self.params:
-                oprot.writeString(iter6.encode('utf-8') if sys.version_info[0] == 2 else iter6)
-            oprot.writeListEnd()
-            oprot.writeFieldEnd()
-        oprot.writeFieldStop()
-        oprot.writeStructEnd()
-
-    def validate(self):
-        return
-
-    def __repr__(self):
-        L = ['%s=%r' % (key, value)
-             for key, value in self.__dict__.items()]
-        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
-
-    def __ne__(self, other):
-        return not (self == other)
+# class Condition(object):
+#     """
+#     Attributes:
+#      - conditionType
+#      - condition
+#      - params
+#     """
+#
+#     thrift_spec = (
+#         None,  # 0
+#         (1, TType.I32, 'conditionType', None, None, ),  # 1
+#         (2, TType.STRUCT, 'condition', (Condition, Condition.thrift_spec), None, ),  # 2
+#         (3, TType.LIST, 'params', (TType.STRING, 'UTF8', False), None, ),  # 3
+#     )
+#
+#     def __init__(self, conditionType=None, condition=None, params=None,):
+#         self.conditionType = conditionType
+#         self.condition = condition
+#         self.params = params
+#
+#     def read(self, iprot):
+#         if iprot._fast_decode is not None and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None:
+#             iprot._fast_decode(self, iprot, (self.__class__, self.thrift_spec))
+#             return
+#         iprot.readStructBegin()
+#         while True:
+#             (fname, ftype, fid) = iprot.readFieldBegin()
+#             if ftype == TType.STOP:
+#                 break
+#             if fid == 1:
+#                 if ftype == TType.I32:
+#                     self.conditionType = iprot.readI32()
+#                 else:
+#                     iprot.skip(ftype)
+#             elif fid == 2:
+#                 if ftype == TType.STRUCT:
+#                     self.condition = Condition()
+#                     self.condition.read(iprot)
+#                 else:
+#                     iprot.skip(ftype)
+#             elif fid == 3:
+#                 if ftype == TType.LIST:
+#                     self.params = []
+#                     (_etype3, _size0) = iprot.readListBegin()
+#                     for _i4 in range(_size0):
+#                         _elem5 = iprot.readString().decode('utf-8') if sys.version_info[0] == 2 else iprot.readString()
+#                         self.params.append(_elem5)
+#                     iprot.readListEnd()
+#                 else:
+#                     iprot.skip(ftype)
+#             else:
+#                 iprot.skip(ftype)
+#             iprot.readFieldEnd()
+#         iprot.readStructEnd()
+#
+#     def write(self, oprot):
+#         if oprot._fast_encode is not None and self.thrift_spec is not None:
+#             oprot.trans.write(oprot._fast_encode(self, (self.__class__, self.thrift_spec)))
+#             return
+#         oprot.writeStructBegin('Condition')
+#         if self.conditionType is not None:
+#             oprot.writeFieldBegin('conditionType', TType.I32, 1)
+#             oprot.writeI32(self.conditionType)
+#             oprot.writeFieldEnd()
+#         if self.condition is not None:
+#             oprot.writeFieldBegin('condition', TType.STRUCT, 2)
+#             self.condition.write(oprot)
+#             oprot.writeFieldEnd()
+#         if self.params is not None:
+#             oprot.writeFieldBegin('params', TType.LIST, 3)
+#             oprot.writeListBegin(TType.STRING, len(self.params))
+#             for iter6 in self.params:
+#                 oprot.writeString(iter6.encode('utf-8') if sys.version_info[0] == 2 else iter6)
+#             oprot.writeListEnd()
+#             oprot.writeFieldEnd()
+#         oprot.writeFieldStop()
+#         oprot.writeStructEnd()
+#
+#     def validate(self):
+#         return
+#
+#     def __repr__(self):
+#         L = ['%s=%r' % (key, value)
+#              for key, value in self.__dict__.items()]
+#         return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+#
+#     def __eq__(self, other):
+#         return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+#
+#     def __ne__(self, other):
+#         return not (self == other)
 
 
 class WordpressPosts(object):
