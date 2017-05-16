d62059644333eac572ed8775d81b3ef6f66b1158
Panda <panyuxin@moseeker.com>
2017-05-16 17:30:35 +0800

    调整团队信息

--- handler/qx/team.py
+++ handler/qx/team.py
@@ -93,6 +93,6 @@ class TeamDetailHandler(BaseHandler):
 
             if template.get("type", 0) == 3:
                 # 不需要团队在招职位
-                template.clear()
+                template.pop()
 
         return templates, share_cover
