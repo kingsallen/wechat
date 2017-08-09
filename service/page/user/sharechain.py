12ca1f51230d68fdcad93028228395b5cd7da7b6
wangyaofeng <wangyaofeng@moseeker.com>
2017-08-09 18:08:27 +0800

    limit 1

--- service/page/user/sharechain.py
+++ service/page/user/sharechain.py
@@ -281,7 +281,8 @@ class SharechainPageService(PageService):
                 "presentee_user_id": user_id,
                 "click_time": [fixed_now, "<="]
             },
-            appends=['ORDER BY click_time DESC']
+            appends=['ORDER BY click_time DESC',
+                     'LIMIT 1']
         )
         raise gen.Return(record)
 
