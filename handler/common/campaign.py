be2434ddcdb2b2d1c995aa8a4893f18514323ffa
chendi <joker_chendi@126.com>
2017-10-12 21:01:55 +0800

    不需要写这坨页面缓存，而且逻辑也不对，直接使用项目里面的cache即可

--- handler/common/campaign.py
+++ handler/common/campaign.py
@@ -6,59 +6,16 @@ from tornado import gen
 
 import conf.common as const
 import conf.path as path
-from globals import redis
 from handler.metabase import MetaBaseHandler
 from util.common import ObjectDict
+from util.common.decorator import cache
 from util.common.decorator import handle_response
 from util.tool.url_tool import make_url
 
 CacheConfig = namedtuple("CacheConfig", ["use", "ttl", "cache_key"])
 
 
-class CacheRenderMixin:
-    def cache_render(self, template_name, cache_config, **kwargs):
-        self.config_cache(cache_config)
-        if self.cache_config.use:
-            cached_html = self.get_page()
-            if cached_html:
-                self.logger.debug("got page from cache, key: {}".format(self.cache_config.cache_key))
-                html = cached_html
-            else:
-                html = self.render_string(template_name, **kwargs)
-                self.save_page(html)
-            self.finish(html)
-        else:
-            self.render(template_name, **kwargs)
-
-    def save_page(self, html):
-        self.logger.debug("save page to cache, key: {}".format(self.cache_config.cache_key))
-        try:
-            redis.set(key=self.cache_config.cache_key, value=html, ttl=self.cache_config.ttl)
-        except Exception as e:
-            self.logger.error(e)
-
-    def get_page(self):
-        try:
-            cached_html = redis.get(self.cache_config.cache_key)
-        except Exception as e:
-            self.logger.error(e)
-            return None
-        else:
-            return cached_html
-
-    def get_cache_render(self):
-        return self
-
-    def config_cache(self, conf: CacheConfig):
-        """
-        设置缓存配置, 不直接放在self下, 用一个属性来存防止命名冲突
-        :param conf:
-        :return: None
-        """
-        self.cache_config = conf
-
-
-class AlipayCampaignCompanyHandler(MetaBaseHandler, CacheRenderMixin):
+class AlipayCampaignCompanyHandler(MetaBaseHandler):
     def make_url(self, path, params=None, host="", protocol="https", escape=None, **kwargs):
         if not host:
             host = self.host
@@ -75,9 +32,9 @@ class AlipayCampaignCompanyHandler(MetaBaseHandler, CacheRenderMixin):
         namespace.update(add_namespace)
         return namespace
 
-    @handle_response
+    @cache(ttl=60*60)
     @gen.coroutine
-    def get(self, campaign_id):
+    def _get(self, campaign_id):
         """
         支付宝活动Landing页, 公司
         :param campaign_id: 活动id
@@ -85,15 +42,21 @@ class AlipayCampaignCompanyHandler(MetaBaseHandler, CacheRenderMixin):
         """
         campaign = yield self.campaign_ps.get_alipay_campaign_by_id(campaign_id)
         if not campaign:
-            self.write_error(http_code=404)
+            return None
         else:
-            cache_config = CacheConfig(use=False,
-                                       ttl=60 * 60,
-                                       cache_key="alipay_campaign_company:{}".format(campaign_id))
-            self.cache_render(template_name="h5/alipay/index.html", cache_config=cache_config, campaign=campaign)
+            return self.render_string(template_name="h5/alipay/index.html", campaign=campaign)
+
+    @handle_response
+    @gen.coroutine
+    def get(self, campaign_id):
+        page_content = yield self._get(campaign_id)
+        if not page_content:
+            self.write_error(404)
+        else:
+            self.finish(page_content)
 
 
-class AlipayCampaignPositionHandler(MetaBaseHandler, CacheRenderMixin):
+class AlipayCampaignPositionHandler(MetaBaseHandler):
     def make_url(self, path, params=None, host="", protocol="https", escape=None, **kwargs):
         if not host:
             host = self.host
@@ -110,9 +73,9 @@ class AlipayCampaignPositionHandler(MetaBaseHandler, CacheRenderMixin):
         namespace.update(add_namespace)
         return namespace
 
-    @handle_response
+    @cache(ttl=60*60)
     @gen.coroutine
-    def get(self, campaign_id):
+    def _get(self, campaign_id):
         """
         支付宝活动的多个公司的职位列表页面
         :param campaign_id: 活动id
@@ -120,9 +83,15 @@ class AlipayCampaignPositionHandler(MetaBaseHandler, CacheRenderMixin):
         """
         campaign = yield self.campaign_ps.get_alipay_campaign_by_id(campaign_id)
         if not campaign:
-            self.write_error(http_code=404)
+            return None
         else:
-            cache_config = CacheConfig(use=False,
-                                       ttl=60 * 60,
-                                       cache_key="alipay_campaign_position:{}".format(campaign_id))
-            self.cache_render(template_name="h5/alipay/list.html", cache_config=cache_config, campaign=campaign)
+            return self.render_string(template_name="h5/alipay/list.html", campaign=campaign)
+
+    @handle_response
+    @gen.coroutine
+    def get(self, campaign_id):
+        page_content = yield self._get(campaign_id)
+        if not page_content:
+            self.write_error(404)
+        else:
+            self.finish(page_content)
