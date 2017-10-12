2eda30e4bbb3cd3403c0fd8a42de06b74902a1c1
chendi <joker_chendi@126.com>
2017-10-12 21:11:45 +0800

    debug, redis_key名字重复

--- handler/common/campaign.py
+++ handler/common/campaign.py
@@ -34,7 +34,7 @@ class AlipayCampaignCompanyHandler(MetaBaseHandler):
 
     @cache(ttl=60*60)
     @gen.coroutine
-    def _get(self, campaign_id):
+    def _get_alipay_campaign_company_page(self, campaign_id):
         """
         支付宝活动Landing页, 公司
         :param campaign_id: 活动id
@@ -49,7 +49,7 @@ class AlipayCampaignCompanyHandler(MetaBaseHandler):
     @handle_response
     @gen.coroutine
     def get(self, campaign_id):
-        page_content = yield self._get(campaign_id)
+        page_content = yield self._get_alipay_campaign_company_page(campaign_id)
         if not page_content:
             self.write_error(404)
         else:
@@ -75,7 +75,7 @@ class AlipayCampaignPositionHandler(MetaBaseHandler):
 
     @cache(ttl=60*60)
     @gen.coroutine
-    def _get(self, campaign_id):
+    def _get_alipay_campaign_position_page(self, campaign_id):
         """
         支付宝活动的多个公司的职位列表页面
         :param campaign_id: 活动id
@@ -90,7 +90,7 @@ class AlipayCampaignPositionHandler(MetaBaseHandler):
     @handle_response
     @gen.coroutine
     def get(self, campaign_id):
-        page_content = yield self._get(campaign_id)
+        page_content = yield self._get_alipay_campaign_position_page(campaign_id)
         if not page_content:
             self.write_error(404)
         else:
