34ce7b57518d8e6ca7ab7a0c9cfc614ecd98a0d8
niuzhenya <niuzhenya@niuzhenyadeMac-mini.local>
2018-01-03 10:54:11 +0800

    高级搜索链接配置条件对筛选条件差生影响

--- service/page/job/landing.py
+++ service/page/job/landing.py
@@ -46,24 +46,92 @@ class LandingPageService(PageService):
         return key_list
 
     @gen.coroutine
-    def get_positions_data(self, conf_search_seq, company_id):
+    def get_positions_data(self, conf_search_seq, company_id, search_condition_dict):
         """ 从 ES 获取全部职位信息
         可以正确解析 salary
         """
         ret = []
         query_size = platform_const.LANDING_QUERY_SIZE
 
-        data = {
-            "size": query_size,
-            "query": {
-                "bool": {
-                    "must": [
-                        {"match": {"company_id": company_id}},
-                        {"match": {"status": const.OLD_YES}}
-                        ]
+        key_list = []
+        value_list = []
+        # 默认最多可以附带三个链接筛选条件
+        if search_condition_dict:
+            for key, value in search_condition_dict.items():
+                key_list.append(key)
+                value_list.append(value)
+            if len(key_list) == 1:
+                key_a, value_a = key_list[0], value_list[0]
+                data = {
+                    "size": query_size,
+                    "query": {
+                        "bool": {
+                            "must": [
+                                {"match": {"company_id": company_id}},
+                                {"match": {"status": const.OLD_YES}},
+                                {"match": {key_a: value_a}}
+                            ]
+                        }
                     }
                 }
-            }
+            elif len(key_list) == 2:
+                key_a, value_a = key_list[0], value_list[0]
+                key_b, value_b = key_list[1], value_list[1]
+                data = {
+                    "size": query_size,
+                    "query": {
+                        "bool": {
+                            "must": [
+                                {"match": {"company_id": company_id}},
+                                {"match": {"status": const.OLD_YES}},
+                                {"match": {key_a: value_a}},
+                                {"match": {key_b: value_b}}
+                            ]
+                        }
+                    }
+                }
+            elif len(key_list) == 3:
+                key_a, value_a = key_list[0], value_list[0]
+                key_b, value_b = key_list[1], value_list[1]
+                key_c, value_c = key_list[2], value_list[2]
+                data = {
+                    "size": query_size,
+                    "query": {
+                        "bool": {
+                            "must": [
+                                {"match": {"company_id": company_id}},
+                                {"match": {"status": const.OLD_YES}},
+                                {"match": {key_a: value_a}},
+                                {"match": {key_b: value_b}},
+                                {"match": {key_c: value_c}}
+                            ]
+                        }
+                    }
+                }
+            else:
+                data = {
+                    "size": query_size,
+                    "query": {
+                        "bool": {
+                            "must": [
+                                {"match": {"company_id": company_id}},
+                                {"match": {"status": const.OLD_YES}}
+                            ]
+                        }
+                    }
+                }
+        else:
+            data = {
+                "size": query_size,
+                "query": {
+                    "bool": {
+                        "must": [
+                            {"match": {"company_id": company_id}},
+                            {"match": {"status": const.OLD_YES}}
+                            ]
+                        }
+                    }
+                }
         response = self.es.search(index='index', body=data)
 
         result_list = response.hits.hits
@@ -144,7 +212,7 @@ class LandingPageService(PageService):
         return data
 
     @gen.coroutine
-    def make_search_seq(self, company):
+    def make_search_seq(self, company, params):
         """
         生成高级搜索功能中前端需要的数据
         :param company:
@@ -157,6 +225,15 @@ class LandingPageService(PageService):
         """
         conf_search_seq = tuple([int(e.index) for e in company.get("conf_search_seq")])
 
+        key_order = [platform_const.LANDING[kn].get("display_key") for kn in conf_search_seq]
+
+        # 获取链接上配置的筛选参数
+        search_condition_dict = dict()
+        all_key_order = [platform_const.LANDING[e].get('form_name') for e in range(0, 9)]
+        for key, value in params.items():
+            if value and key in all_key_order and key not in key_order:
+                search_condition_dict.update(key=value)
+
         # 默认 conf_search_seq
         if not conf_search_seq:
             conf_search_seq = (
@@ -166,7 +243,7 @@ class LandingPageService(PageService):
                 platform_const.LANDING_INDEX_DEPARTMENT
             )
 
-        positions_data = yield self.get_positions_data(conf_search_seq, company.id)
+        positions_data = yield self.get_positions_data(conf_search_seq, company.id, search_condition_dict)
 
         if platform_const.LANDING_INDEX_CITY in conf_search_seq:
             positions_data = self.split_cities(positions_data)
@@ -175,8 +252,6 @@ class LandingPageService(PageService):
             positions_data = yield self.append_child_company_name(positions_data)
 
         self.logger.debug(conf_search_seq)
-
-        key_order = [platform_const.LANDING[kn].get("display_key") for kn in conf_search_seq]
 
         # 构建 [{"text": XXX, "value": XXX}, ...] 的形式
         positions_data_values = []
@@ -193,7 +268,17 @@ class LandingPageService(PageService):
                     to_append.append({"text": e.get(k), "value": const.EMPLOYMENT_TYPE_SEARCH_REVERSE.get(e.get(k))})
                 else:
                     to_append.append({"text": e.get(k), "value": e.get(k)})
+            for s in search_condition_dict:
+                if s == 'child_company_abbr':
+                    to_append.append(e.get(s))
 
+                elif s == 'candidate_source_name':
+                    to_append.append({"text": e.get(s), "value": const.CANDIDATE_SOURCE_SEARCH_REVERSE.get(e.get(s))})
+
+                elif s == 'employment_type_name':
+                    to_append.append({"text": e.get(s), "value": const.EMPLOYMENT_TYPE_SEARCH_REVERSE.get(e.get(s))})
+                else:
+                    to_append.append({"text": e.get(s), "value": e.get(s)})
             positions_data_values.append(to_append)
 
         # 将构建出来的结果去重，作为返回中的 values 属性
