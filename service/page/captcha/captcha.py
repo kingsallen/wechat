73c341bbe48c39cc73362bbd4207ffbf8e7bc96b
niuzhenya <niuzhenya@niuzhenyadeMac-mini.local>
2018-01-25 12:06:12 +0800

    up

--- service/page/captcha/captcha.py
+++ service/page/captcha/captcha.py
@@ -11,18 +11,20 @@ class CaptchaPageService(PageService):
         super().__init__()
 
     @gen.coroutine
-    def post_verification(self, captcha, channel, account_id):
+    def post_verification(self, captcha, channel, account_id, position_id):
         """
         :param captcha: 回流的平台编号。1 前程无忧，2 猎聘，3 智联， 6 最佳东方， 7 一览英才
         :param channel: 验证码
         :param account_id: 第三方渠道账号ID
+        :param position_id:职位ID
         :return:
         """
 
         req = ObjectDict({
             'info': captcha,
             'channel': channel,
-            'accountId': account_id
+            'accountId': account_id,
+            'positionId': position_id
         })
         res = yield self.infra_captcha_ds.post_verification(req)
         status, message = res.status, res.message
@@ -41,8 +43,9 @@ class CaptchaPageService(PageService):
         ret = ObjectDict()
         if data:
             ret['channel'] = data.channel
-            ret['accountId'] = data.accounrId
+            ret['accountId'] = data.accountId
             ret['mobile'] = data.mobile
+            ret['positionId'] = data.positionId
         return message, ret
 
 
