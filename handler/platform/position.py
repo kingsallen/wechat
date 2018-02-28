7d496fb1a70a9dcaab5c66c364f44ed43c5c6a73
niuzhenya <niuzhenya@niuzhenyadeMac-mini.local>
2018-02-28 17:04:29 +0800

    注释掉sug

--- handler/platform/position.py
+++ handler/platform/position.py
@@ -1095,16 +1095,21 @@ class PositionListSugHandler(PositionListInfraParamsMixin, BaseHandler):
         sug搜索
         :return:
         """
-        infra_params = self.make_position_list_infra_params()
+        # todo:注释
         sug = ObjectDict()
-        # 获取五条sug数据
-        infra_params.update(page_size=const_platform.SUG_LIST_COUNT,
-                            keyWord=self.params.keyword if self.params.keyword else "",
-                            page_from=int(self.params.get("count", 0)/10) + 1)
-        res_data = yield self.position_ps.infra_obtain_sug_list(infra_params)
-        suggest = res_data.get('suggest') if res_data else ''
-        if suggest:
-            sug.list = [e.get('title') for e in suggest]
-
+        sug.list = []
         return self.send_json_success(sug)
+        
+        # infra_params = self.make_position_list_infra_params()
+        # sug = ObjectDict()
+        # # 获取五条sug数据
+        # infra_params.update(page_size=const_platform.SUG_LIST_COUNT,
+        #                     keyWord=self.params.keyword if self.params.keyword else "",
+        #                     page_from=int(self.params.get("count", 0)/10) + 1)
+        # res_data = yield self.position_ps.infra_obtain_sug_list(infra_params)
+        # suggest = res_data.get('suggest') if res_data else ''
+        # if suggest:
+        #     sug.list = [e.get('title') for e in suggest]
+        #
+        # return self.send_json_success(sug)
 
