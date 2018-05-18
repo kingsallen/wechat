1c833008fec9848ea89b1c0c9fe5920a56d2d404
niuzhenya <niuzhenya@niuzhenyadeMac-mini.local>
2018-05-18 16:32:15 +0800

    修改thrift 文件

--- thrift_gen/defitions/chat/service/chat_service.thrift
+++ thrift_gen/defitions/chat/service/chat_service.thrift
@@ -16,7 +16,7 @@ service ChatService {
     //列出聊天记录
     chat_struct.ChatsVO listChatLogs(1: i32 roomId, 2: i32 pageNo, 3: i32 pageSize) throws (1: common_struct.CURDException e)
     //保存聊天记录
-    i32 saveChat(1: chat_struct.ChatVO chat) throws (1: common_struct.CURDException e)
+    i32 saveChat(1: chat_struct.ChatVO chat) throws (1: common_struct.BIZException e)
     //获取聊天记录
     chat_struct.ChatVO getChat(1: i32 roomId, 2: i8 speaker) throws (1: common_struct.CURDException e)
     //获取聊天室最后一条聊天内容
