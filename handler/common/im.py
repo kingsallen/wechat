d5a1cd5f8ebc5d4457e22e213ff32cc38f83f9cd
牛振亚 <niuzhenya@moseeker.com>
2018-06-27 10:02:27 +0800

    捕获ai聊天异常

--- handler/common/im.py
+++ handler/common/im.py
@@ -483,12 +483,14 @@ class ChatHandler(BaseHandler):
         ))
 
         self.redis_client.publish(self.hr_channel, message_body)
-
-        if self.bot_enabled and msg_type != "job":
-            # 由于没有延迟的发送导致hr端轮训无法订阅到publish到redis的消息　所以这里做下延迟处理
-            # delay_robot = functools.partial(self._handle_chatbot_message, user_message)
-            # ioloop.IOLoop.current().call_later(1, delay_robot)
-            yield self._handle_chatbot_message(user_message)  # 直接调用方式
+        try:
+            if self.bot_enabled and msg_type != "job":
+                # 由于没有延迟的发送导致hr端轮训无法订阅到publish到redis的消息　所以这里做下延迟处理
+                # delay_robot = functools.partial(self._handle_chatbot_message, user_message)
+                # ioloop.IOLoop.current().call_later(1, delay_robot)
+                yield self._handle_chatbot_message(user_message)  # 直接调用方式
+        except Exception as e:
+            self.logger.error(e)
 
         self.send_json_success()
 
