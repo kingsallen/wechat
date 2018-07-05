bbd5ed0ca15a496da00336a28fc9b10069d31d00
牛振亚 <niuzhenya@moseeker.com>
2018-07-05 18:16:01 +0800

    Mars EDM活动

---
+++ dao/campaign/campaign_mars_edm_subscribe.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+# Copyright 2016 MoSeeker
+
+from dao.base import BaseDao
+
+
+class CampaignMarsEdmSubscribeDao(BaseDao):
+
+    def __init__(self):
+        super(CampaignMarsEdmSubscribeDao, self).__init__()
+        self.table = "CampaignMarsEdmSubscribe"
+        self.fields_map = {
+            "id":           self.constant.TYPE_INT,
+            "name":         self.constant.TYPE_STRING,
+            "mobile":       self.constant.TYPE_INT,
+            "email":        self.constant.TYPE_STRING,
+            "wxuser_id":    self.constant.TYPE_INT,
+            "update_time":  self.constant.TYPE_TIMESTAMP,
+            "create_time":  self.constant.TYPE_TIMESTAMP
+        }
