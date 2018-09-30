9b541f56329af34a93ca2d3e8ea49ddf1d5a3269
牛振亚 <niuzhenya@moseeker.com>
2018-09-30 18:48:33 +0800

    up

--- conf/path.py
+++ conf/path.py
@@ -97,7 +97,7 @@ INFRA_USER_EMPLOYEE_CHECK = 'user/employee/check'
 INFRA_USER_EMPLOYEE = 'user/employee'
 INFRA_USER_APPLYRECORD = "/v1/applications"
 INFRA_USER_EMPLOYEE_REFERRAL = "/v1/referral/users/{}/employee-info"
-INFRA_USER_REDPACKET_LIST = "/v1/referral/users/{}/redpackages"
+INFRA_USER_REDPACKET_LIST = "/v1/referral/users/{}/redpackets"
 INFRA_USER_BONUS_LIST = "/v1/referral/users/{}/bonus"
 INFRA_USER_CLAIM_BONUS = "/v1/referral/wechat/employee/{}/bonus/claim"
 
