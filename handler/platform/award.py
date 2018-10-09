01a2625c40d9c415c3187e29c9b8d46ba1994668
牛振亚 <niuzhenya@moseeker.com>
2018-10-09 19:22:18 +0800

    修改红包列表时间

--- handler/platform/award.py
+++ handler/platform/award.py
@@ -44,7 +44,7 @@ class ReferralRedpacketHandler(BaseHandler):
         ret = yield self.user_ps.get_redpacket_list(self.current_user.sysuser.id, params)
         list = ret.redpackets
         for i in list:
-            open_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(i.get("open_time")))
+            open_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(i.get("open_time", 0))/1000))
             i['open_time'] = open_time
             i['name'] = self.locale.translate(const.REDPACKET.get(i.get("type")))
         data = ObjectDict(list=list)
@@ -66,7 +66,7 @@ class ReferralBonusHandler(BaseHandler):
         ret = yield self.user_ps.get_bonus_list(self.current_user.sysuser.id, params)
         list = ret.bonus
         for i in list:
-            open_time = time.strftime('%Y-%m-%d', time.localtime(i.get("open_time")))
+            open_time = time.strftime('%Y-%m-%d', time.localtime(int(i.get("open_time", 0))/1000))
             i['open_time'] = open_time
             i['type'] = 3  # 入职奖金
             if i.get("cancel"):
