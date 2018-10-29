1d0646358532e0ab673ff8f97aec75092fdc52ee
yss <ysongs@songs.local>
2018-10-29 14:49:08 +0800

    add log

--- service/data/infra/infra_profile.py
+++ service/data/infra/infra_profile.py
@@ -647,6 +647,7 @@ class InfraProfileDataService(DataService):
             "id": record.id,
             "profile_id": profile_id
         }
+        self.logger.debug("update_profile_intention:record--->{}".format(record))
         if record.get('city_name'):
             params.update({"city": record.city_name})
         if record.get('position_name'):
@@ -664,15 +665,9 @@ class InfraProfileDataService(DataService):
             for i in range(len(industry)):
                 params.update({"industries[{}]industry_name".format(i) : industry[i]['industry_name']})
                 params.update({"industries[{}]industry_code".format(i) : industry[i]['industry_code']})
-            # for item in record.get('industry'):
-            #     index = 0
-            #     params.update({"industries[{}]industry_name".format(index) : item.get("industry_name")})
-            #     params.update({"industries[{}]industry_code".format(index) : item.get("industry_code")})
-            #     index += 1
         if record.get('workstate'):
             params.update({"workstate": record.workstate})
         self.logger.debug("update_profile:position_name----->{}".format(record.get('position_name')))
-        self.logger.debug("update_profile:industries----->{}".format(record.get('industry')))
         self.logger.debug("update_profile:param======>{}".format(params))
 
         res = yield self.handle_profile_section(
