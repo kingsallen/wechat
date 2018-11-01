4f4ba88b3cef88dae354f4f729fa5a12de05ba54
牛振亚 <niuzhenya@moseeker.com>
2018-11-01 15:40:59 +0800

    es搜索时occupation做特殊处理

--- service/page/job/landing.py
+++ service/page/job/landing.py
@@ -45,6 +45,7 @@ class LandingPageService(PageService):
         """ 从 ES 获取全部职位信息
         可以正确解析 salary
         """
+        # todo es的搜索语句重构掉，使用dict的update，不要直接写死dict
         ret = []
         query_size = platform_const.LANDING_QUERY_SIZE
 
@@ -62,6 +63,17 @@ class LandingPageService(PageService):
                 }
             }
         }
+        # occupation使用term搜索
+        pop_occupation = False
+        occupation_value = ''
+        if search_condition_dict:
+            for key, value in search_condition_dict.items():
+                if key == 'occupation':
+                    pop_occupation = True
+                    occupation_value = value
+            if pop_occupation:
+                search_condition_dict.pop("occupation")
+
         # 默认最多可以附带三个链接筛选条件
         if search_condition_dict and not salary_dict:
             for key, value in search_condition_dict.items():
@@ -183,6 +195,8 @@ class LandingPageService(PageService):
             }
         if is_referral:
             data.get("query").get("bool").get("must").append({"match": {"is_referral": const.YES}})
+        if pop_occupation:
+            data.get("query").get("bool").get("must").append({"term": {"search_data.occupation": occupation_value}})
         self.logger.debug(data)
         response = self.es.search(index='index', body=data)
 
