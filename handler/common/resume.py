ad06faa9c27bbfa7fc1d5fb7972316a5c6ac95e6
CosimoZi <xzyufo@gmail.com>
2018-11-07 17:48:10 +0800

    Minor fix.

--- handler/common/resume.py
+++ handler/common/resume.py
@@ -393,7 +393,9 @@ class ChatbotResumeUploadHandler(BaseHandler):
             }
             self.logger.debug('data for rendering is %s' % data)
             self.params.share = yield self._make_share(
-                dict(rid=','.join(str(i) for i in rids), pid=','.join(str(i) for i in pids)))
+                dict(rid=','.join(str(i) for i in rids),
+                     pid=','.join(str(i) for i in pids),
+                     wechat_signature=self.current_user.wechat.signature))
         self.render_page(template_name="chat/mobot-upload-resume.html", data=data)
 
     @gen.coroutine
