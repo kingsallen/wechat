1eb4058f4ccac6322c8736edf1f2e7833766b4d5
牛振亚 <niuzhenya@moseeker.com>
2018-12-14 18:48:22 +0800

    up

--- handler/platform/radar_demo.py
+++ handler/platform/radar_demo.py
@@ -26,60 +26,59 @@ class RadarDemoApiHandler(BaseHandler):
         num = self.params.num - 5
         position_id = self.params.position_id
         position = self.position_ps.get_position(position_id=position_id)
-        global position
         record = self.sharechain_ps.find_candidate_by_position(position_id=position_id, num=num)
+
+        hack_data = [
+            {
+                "id": 111111,
+                "name": "陈迪",
+                "headimg": "http://thirdwx.qlogo.cn/mmopen/Q3auHgzwzM5bdNZA3dQ6Ddj47omFnwC9oZTDy1ibpOmOg2LsQKE5291EHaddBKOkeicakiaNrb1XQF7jJfWBkJzog/132",
+                "is_hack": True,
+                "viewnum": 9,
+                "click_from": 2,
+                "click_time": "2018-12-20 07:12:43",
+                "position_title": "{}".format(position.title)},
+            {
+                "id": 111112,
+                "name": "Towry",
+                "headimg": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJ1Q15bKpUb0vXGCgLjYHpiaBDGZohOrqh1TUKvS1IAusibRtk6mUWF6AMcLfjReHz2O3uO4XJWVcpw/132",
+                "is_hack": True,
+                "viewnum": 3,
+                "click_from": 1,
+                "click_time": "2018-12-20 08:32:46",
+                "position_title": "{}".format(position.title)
+            },
+            {
+                "id": 111113,
+                "name": "Sunny",
+                "headimg": "https://thirdwx.qlogo.cn/mmopen/vi_32/8hN3qjD08YAmwiashKFhZQEN18WaqaKQygThnSWpNicYIZroucggHxm2x3pk6TMtnOzQh3WpBvIGXHpd6mwXiag7w/132",
+                "is_hack": True,
+                "viewnum": 2,
+                "click_from": 1,
+                "click_time": "2018-12-20 08:45:32",
+                "position_title": "{}".format(position.title)
+            },
+            {
+                "id": 111114,
+                "name": "Jack Weng",
+                "headimg": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTK3HJD1CfyeTuJpSgUK2p2aVGd4FA2BXIiaTvmxcyWrzbsEUhb532ZNP0pMEAz3BUPl8PAh9jRnweg/132",
+                "is_hack": True,
+                "viewnum": 4,
+                "click_from": 3,
+                "click_time": "2018-12-20 08:20:33",
+                "position_title": "{}".format(position.title)
+            },
+            {
+                "id": 111115,
+                "name": "Neil",
+                "headimg": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLMcEQkPTX3gF6bodiat42ic4u3icSkia8iaYlFOo5NS4E3tiap5UDEsCZHgo2Rww2MmKl5pvkC9n7icWtIQ/132",
+                "is_hack": True,
+                "viewnum": 1,
+                "click_from": 3,
+                "click_time": "2018-12-20 08:11:15",
+                "position_title": "{}".format(position.title)
+            },
+        ]
+
         record = hack_data + record
         self.send_json_success(record)
-
-
-hack_data = [
-    {
-        "id": 111111,
-        "name": "陈迪",
-        "headimg": "http://thirdwx.qlogo.cn/mmopen/Q3auHgzwzM5bdNZA3dQ6Ddj47omFnwC9oZTDy1ibpOmOg2LsQKE5291EHaddBKOkeicakiaNrb1XQF7jJfWBkJzog/132",
-        "is_hack": True,
-        "viewnum": 9,
-        "click_from": 2,
-        "click_time": "2018-12-20 07:12:43",
-        "position_title": "{}".format(position.title)},
-    {
-        "id": 111112,
-        "name": "Towry",
-        "headimg": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJ1Q15bKpUb0vXGCgLjYHpiaBDGZohOrqh1TUKvS1IAusibRtk6mUWF6AMcLfjReHz2O3uO4XJWVcpw/132",
-        "is_hack": True,
-        "viewnum": 3,
-        "click_from": 1,
-        "click_time": "2018-12-20 08:32:46",
-        "position_title": "{}".format(position.title)
-    },
-    {
-        "id": 111113,
-        "name": "Sunny",
-        "headimg": "https://thirdwx.qlogo.cn/mmopen/vi_32/8hN3qjD08YAmwiashKFhZQEN18WaqaKQygThnSWpNicYIZroucggHxm2x3pk6TMtnOzQh3WpBvIGXHpd6mwXiag7w/132",
-        "is_hack": True,
-        "viewnum": 2,
-        "click_from": 1,
-        "click_time": "2018-12-20 08:45:32",
-        "position_title": "{}".format(position.title)
-    },
-    {
-        "id": 111114,
-        "name": "Jack Weng",
-        "headimg": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTK3HJD1CfyeTuJpSgUK2p2aVGd4FA2BXIiaTvmxcyWrzbsEUhb532ZNP0pMEAz3BUPl8PAh9jRnweg/132",
-        "is_hack": True,
-        "viewnum": 4,
-        "click_from": 3,
-        "click_time": "2018-12-20 08:20:33",
-        "position_title": "{}".format(position.title)
-    },
-    {
-        "id": 111115,
-        "name": "Neil",
-        "headimg": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLMcEQkPTX3gF6bodiat42ic4u3icSkia8iaYlFOo5NS4E3tiap5UDEsCZHgo2Rww2MmKl5pvkC9n7icWtIQ/132",
-        "is_hack": True,
-        "viewnum": 1,
-        "click_from": 3,
-        "click_time": "2018-12-20 08:11:15",
-        "position_title": "{}".format(position.title)
-    },
-]
