d0d8cd56155da043b37fa306eebfd685fd5dbad7
牛振亚 <niuzhenya@moseeker.com>
2019-02-19 16:19:19 +0800

    up

--- service/page/redpacket/redpacket.py
+++ service/page/redpacket/redpacket.py
@@ -538,6 +538,7 @@ class RedpacketPageService(PageService):
             if not recom_user_id:
                 self.logger.debug('[RP]推荐数据不正确, position: %s, app_id: %s, presentee_user_id: %s, post_user_id: %s'
                                       % (position.id, application.id, current_user.sysuser.id, recom_user_id))
+                return
 
             recom_record = yield self.candidate_recom_record_ds.get_candidate_recom_record(params)
 
