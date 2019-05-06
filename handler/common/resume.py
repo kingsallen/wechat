ee10bc14d8b14c7f9bc19e8ab700db35bf809fa4
牛振亚 <niuzhenya@moseeker.com>
2019-05-06 10:56:50 +0800

    修改返回数据的格式

--- handler/common/resume.py
+++ handler/common/resume.py
@@ -515,7 +515,7 @@ class ResumeUploadResultHandler(BaseHandler):
         employee_id = self.current_user.employee.id if self.current_user.employee else 0
         result = yield self.profile_ps.is_resume_upload_complete(self.current_user.sysuser.id, sync_id, employee_id)
         if result.status == const.API_SUCCESS:
-            self.send_json_success(result.data)
+            self.send_json_success({"finished": result.data})
         else:
             self.send_json_error(message=result.message)
 
