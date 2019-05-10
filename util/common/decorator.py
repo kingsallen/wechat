0d855c5e55d936dcf78911630c0d1b3fe0fb3d97
牛振亚 <niuzhenya@moseeker.com>
2019-05-10 14:06:27 +0800

    限制详情页访问时状态码为416

--- util/common/decorator.py
+++ util/common/decorator.py
@@ -334,7 +334,7 @@ def cover_no_weixin(func):
     @gen.coroutine
     def wrapper(self, *args, **kwargs):
         if not self.in_wechat and 'moseeker' not in self.request.headers.get('User-Agent'):
-            self.render(template_name="adjunct/not-weixin.html")
+            self.render(template_name="adjunct/not-weixin.html", http_code=416)
             return
         else:
             yield func(self, *args, **kwargs)
