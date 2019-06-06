844569a57614b2d854ad7039f5961f3d505e476d
zhaozhonghua <intasect@126.com>
2019-06-06 11:13:24 +0800

    fix 添加post_message参数注释

--- handler/common/im.py
+++ handler/common/im.py
@@ -486,6 +486,27 @@ class ChatHandler(BaseHandler):
     @authenticated
     @gen.coroutine
     def post_message(self):
+        """
+        用户chat发送消息响应处理
+
+        @:param flag 2:meet mobot, 3:智能推荐, 4:{{data}}, 5: {{decodeURIComponent(data)}}
+                scene emp_chat 我是员工
+        @:param msgType 消息类型  ping, pong, html, text, image, voice, job, voice-preview, cards, job-sender,
+                                 button_radio, teamSelect, textPlaceholder, satisfaction, textList, citySelect,
+                                 industrySelect, positionSelect, jobCard, jobSelect, employeeBind, redirect,
+                                 uploadResume
+        @:param serverId 微信语音内容，微信服务器生成的serverId
+        @:param duration 微信语音时长
+        @:param create_new_context 是否创建了新的会话
+        @:param from_textfield 用户输入内容是否触发脚本非法分支，如触发，终止当前脚本新起脚本（unexpected_branch_allowed）
+        @:param compoundContent 复杂结构体的聊天内容
+        @:param content 用户发送的内容
+        @:param pid 职位ID
+        @:param hrId HRID
+        @:param roomId 聊天室ID
+
+        :return:
+        """
 
         self.room_id = self.params.roomId
         self.user_id = match_session_id(to_str(self.get_secure_cookie(const.COOKIE_SESSIONID)))
