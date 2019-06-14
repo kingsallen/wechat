4d786cd23dd16c93c8cf1e482fcb2aecbf1408c9
huangxia <944421101@qq.com>
2019-06-14 09:32:23 +0800

    update health_check

--- handler/common/health_check.py
+++ handler/common/health_check.py
@@ -2,11 +2,11 @@
 
 from tornado import gen
 from handler.base import BaseHandler
-from util.common.decorator import handle_response
+from util.common.decorator import handle_response,common_handler
 
 
 class HealthcheckHandler(BaseHandler):
-
+    @common_handler
     @handle_response
     @gen.coroutine
     def get(self):
