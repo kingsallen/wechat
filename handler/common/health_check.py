03cce6e11dab1cdad1747fa05d9fface8f50d5f7
huangxia <944421101@qq.com>
2019-06-14 09:39:17 +0800

    update health_check

--- handler/common/health_check.py
+++ handler/common/health_check.py
@@ -5,8 +5,9 @@ from handler.base import BaseHandler
 from util.common.decorator import handle_response,common_handler
 
 
+@common_handler
 class HealthcheckHandler(BaseHandler):
-    @common_handler
+
     @handle_response
     @gen.coroutine
     def get(self):
