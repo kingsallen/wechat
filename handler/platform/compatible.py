23216c2cdc7299a00ed7a04add2e551e804ff3a8
牛振亚 <niuzhenya@moseeker.com>
2019-06-19 15:00:41 +0800

    增加list跳转

--- handler/platform/compatible.py
+++ handler/platform/compatible.py
@@ -8,9 +8,17 @@
 from tornado import gen
 
 from handler.base import BaseHandler
+from util.common.decorator import handle_response
+from conf import path
 
 
 class CompatibleHandler(BaseHandler):
 
+    @handle_response
+    @gen.coroutine
     def get(self):
-        self.write_error(404)
+        if self.request.uri.startswith("/mobile/position") and self.params.m == "list":
+            redirec_url = self.make_url(path=path.POSITION_LIST, params=self.params, host=self.host)
+            self.logger.debug("[mobile position] redirect_url: {}".format(redirec_url))
+            self.redirect(redirec_url)
+
