ddc11764c3f2da98170ff0c471f922eefa762708
huangxia <944421101@qq.com>
2019-06-21 16:38:09 +0800

    update event_scan

--- service/page/wechat/event.py
+++ service/page/wechat/event.py
@@ -542,7 +542,7 @@ class EventPageService(PageService):
         str_scene = ""
         if msg.EventKey:
             # 临时二维码处理逻辑, 5位type+27为自定义id
-            int_scene_id = re.match(r"(\d+)", msg.EventKey)
+            int_scene_id = re.match(r"(\d+)$", msg.EventKey)
             if not int_scene_id:
                 int_scene_id = re.match(r"qrscene_(\d+)", msg.EventKey)
                 # 处理自定义参数为字符串的临时二维码
