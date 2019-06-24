7be24454482903b84d8a4c36fb9a70f84a65082f
牛振亚 <niuzhenya@moseeker.com>
2019-06-24 11:26:53 +0800

    修改继承的类

--- handler/common/health_check.py
+++ handler/common/health_check.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 
 from tornado import gen
-from handler.base import BaseHandler
+from handler.base import MetaBaseHandler
 from util.common.decorator import handle_response,common_handler
 
 
 @common_handler
-class HealthcheckHandler(BaseHandler):
+class HealthcheckHandler(MetaBaseHandler):
 
     @handle_response
     @gen.coroutine
