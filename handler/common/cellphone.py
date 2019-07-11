6ee8ed3d1420d9574c7b7596eee71dccfc1a6124
huangxia <944421101@qq.com>
2019-07-11 20:06:20 +0800

    Merge branch 'feature/changemobile' into dev1
    # Conflicts:
    #	handler/common/cellphone.py

--- handler/common/cellphone.py
+++ handler/common/cellphone.py
@@ -257,7 +257,7 @@ class CellphoneBindHandler(CaptchaMixin, BaseHandler):
         )
 
         if response.code != const.API_SUCCESS:
-            raise InfraOperationError(response.message)
+            self.send_json_error(message=response.message)
 
         ret_user_id = response.data.userid
 
