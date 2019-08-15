3a557ddfb0dfcabe8ac0bc1581bc5938bcbcba07
huangxia <944421101@qq.com>
2019-08-15 20:32:04 +0800

    开发企业微信认证

--- handler/base.py
+++ handler/base.py
@@ -1033,7 +1033,7 @@ class BaseHandler(MetaBaseHandler):
             is_subscribe = yield self.position_ps.get_hr_wx_user(self.current_user.sysuser.unionid, self._wechat.id)
             if is_subscribe:
                 # 如果已经关注公众号，无需跳转微信，可生成员工信息之后访问主页
-                yield self.workwx_ps.employee_bind(self.current_user.sysuser.unionid, self._wechat.company_id)
+                yield self.workwx_ps.employee_bind(self.current_user.sysuser.id, self._wechat.company_id)
             else:
                 # 如果没有关注公众号，跳转微信
                 workwx_user_record = yield self.workwx_ps.get_workwx_user_by_sysuser_id(
