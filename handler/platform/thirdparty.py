f896f3edee85fd319a6323f603bc953398e20bd3
huangxia <944421101@qq.com>
2019-08-27 12:05:29 +0800

    企业微信修复bug:由于增加撤销员工身份会解绑企业微信和微信账号功能，因此必须在员工认证之后再做绑定

--- handler/platform/thirdparty.py
+++ handler/platform/thirdparty.py
@@ -176,6 +176,8 @@ class EmployeeQrcodeHandler(BaseHandler):
         # else:
         #     yield self.workwx_ps.bind_workwx_qxuser(self.current_user.sysuser.id, workwx_userid, company_id)
 
+        # if workwx_user_record:
+        #     if int(workwx_user_record.sys_user_id) > 0 and workwx_user_record.sys_user_id != self.sysuser.id:
         #如果已经关注公众号，说明已经做完员工认证，生成员工信息，跳转3s跳转页，再跳转到职位列表
         if self.current_user.wxuser.is_subscribe:
             is_valid_employee = yield self.employee_ps.is_valid_employee(
@@ -195,7 +197,7 @@ class EmployeeQrcodeHandler(BaseHandler):
             three_sec_wechat_url =  self.make_url(path.WECHAT_THREESEC_PAGE, self.params)
             self.redirect(three_sec_wechat_url)
             return
-        self.render_page(template_name="adjunct/wxwork-qrcode.html", data=ObjectDict())
+        self.render_page(template_name="adjunct/wxwork-qrcode.html", data=ObjectDict({"str_code": workwx_user_record.id}))
 
 
 class WorkwxQrcodeHandler(MetaBaseHandler):
