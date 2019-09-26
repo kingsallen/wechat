348edd59034a8f653448b312eb127871130212a3
牛振亚 <niuzhenya@moseeker.com>
2019-09-26 10:18:26 +0800

    修改发送验证码的默认国家区号为86

--- handler/common/passport.py
+++ handler/common/passport.py
@@ -364,7 +364,7 @@ class SendValidCodeHandler(BaseHandler):
     def post(self):
 
         mobile = self.json_args.mobile
-        country_code = int(self.json_args.country_code or 0)
+        country_code = int(self.json_args.country_code or 86)
         if not mobile:
             mobile = self.redis.get(
                 const.CONFIRM_REFERRAL_MOBILE.format(self.json_args.rkey, self.current_user.sysuser.id)).get("mobile")
