e6d38b675a5134650d6591a39e8a4db7ef87fde3
huangxia <944421101@qq.com>
2019-10-09 11:54:14 +0800

    lbs开发

--- route.py
+++ route.py
@@ -181,7 +181,7 @@ common_routes = [
 platform_routes = [
     (r"/position/(?P<position_id>\d+)",              handler.platform.position.PositionHandler,                 {"event": "position_info"}),
     (r"/position/?",                                 handler.platform.position.PositionListHandler,             {"event": "position_list"},      'position_list'),
-    (r"/lbs/position/?",                             handler.platform.position.LbsPositionHandler,              {"event": "lbs_position_info"}),
+    (r"/lbs/position/?",                             handler.platform.position.PositionListHandler,              {"event": "lbs_position_info"}),
     (r"/position/share?",                            handler.platform.position.PositionShareInBulkHandler,      {"event": "share_position_list"}),
     (r"/start/?",                                    handler.platform.landing.LandingHandler,                   {"event": "start_landing"}),
     (r"/company/(\d+)",                              handler.platform.companyrelation.CompanyInfoRedirectHandler, {"event": "company_old_info"}, "old_company_info_page"),
