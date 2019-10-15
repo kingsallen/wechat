cbd532a66910860b95ea8a773595a35e06d2eef3
huangxia <944421101@qq.com>
2019-10-15 15:27:53 +0800

    lbs开发

--- conf/newinfra_service_conf/company/company.py
+++ conf/newinfra_service_conf/company/company.py
@@ -1,5 +1,5 @@
 # coding=utf-8
 
 COMPANY_HR_INFO = "/v4/hr/info"
-COMPANY_NEARBY_STORES = "/stores/nearby"
-COMPANY_POSITION_LBS = "/stores/nearby/{}"
+COMPANY_NEARBY_STORES = "/v4/stores/nearby"
+COMPANY_POSITION_LBS = "/v4/stores/nearby/{}"
