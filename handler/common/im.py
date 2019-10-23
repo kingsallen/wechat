af9ad2c3ac5645b28e516c7a77adb8f2f9817aa5
jack frame weng <wjf2255@gmail.com>
2019-10-23 19:53:17 +0800

    修复传参

--- handler/common/im.py
+++ handler/common/im.py
@@ -931,8 +931,8 @@ class EmployeeChattingHandler(BaseHandler):
         if self.current_user.employee:
             # 当前是员工，获取与候选人的聊天室列表
             self.role = "employee"
-            self.employee_id = self.params.employee_id
-            self.user_id = 0
+            self.employee_id = self.current_user.employee.id
+            self.user_id = self.params.user_id
         else:
             # 当前用户是普通的候选人，获取公众号所属公司下员工的聊天室列表
             self.role = "user"
@@ -966,11 +966,11 @@ class EmployeeChattingHandler(BaseHandler):
             # 当前是员工，获取与候选人的聊天室列表
             self.role = "employee"
             self.employee_id = self.current_user.employee.id
-            self.user_id = 0
+            self.user_id = self.params.user_id
         else:
             # 当前用户是普通的候选人，获取公众号所属公司下员工的聊天室列表
             self.role = "user"
-            self.employee_id = 0
+            self.employee_id = self.params.employee_id
             self.user_id = self.current_user.sysuser.id
 
         try:
@@ -1053,7 +1053,7 @@ class EmployeeChattingHandler(BaseHandler):
         :return: 推送开关状态
         """
 
-        self.logger.debug("enter room. employee_id:{}", self.employee_id)
+        self.logger.debug("enter room. employee_id:{}".format())
         ret = yield self.chat_ps.enter_the_room(self.params.room_id, self.role, self.user_id, self.employee_id,
                                                 self.current_user.company.id, self.params.position_id)
         self.send_json_success(ret)
