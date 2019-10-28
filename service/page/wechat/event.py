8f5fe5bd3b195c0874adc733f64218c0def91c8a
jack frame weng <wjf2255@gmail.com>
2019-10-28 18:25:38 +0800

    修复

--- service/page/wechat/event.py
+++ service/page/wechat/event.py
@@ -840,7 +840,7 @@ class EventPageService(PageService):
                 position = yield self.job_position_ds.get_position(position_params)
                 company_params = {"id": employee.company_id}
                 _, company = yield self.infra_company_ds.get_company_by_id(company_params)
-                user = yield self.infra_user_ds.get_user(employee.sysuser_id)
+                user = yield self.infra_user_ds.get_user_by_user_id(employee.sysuser_id)
 
                 if not (employee and user and position):
                     return
