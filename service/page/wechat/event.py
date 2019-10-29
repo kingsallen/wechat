fb692488b8c6f95c39528a3d6efa9969bc2a4bbe
jack frame weng <wjf2255@gmail.com>
2019-10-29 20:14:24 +0800

    修复问题

--- service/page/wechat/event.py
+++ service/page/wechat/event.py
@@ -840,15 +840,15 @@ class EventPageService(PageService):
                 position = yield self.job_position_ds.get_position(position_params)
                 company_params = {"id": employee.company_id}
                 _, company = yield self.infra_company_ds.get_company_by_id(company_params)
-                user = yield self.infra_user_ds.get_user_by_user_id(employee.sysuser_id)
+                user = yield self.infra_user_ds.get_user(employee.sysuser_id)
 
-                if not (employee and user and position):
+                if not (employee and user and user.data and position):
                     return
 
                 if isinstance(company, list):
                     company = company[0]
 
-                employee_name = employee.cname or user.name or user.nickname or ""
+                employee_name = employee.cname or user.data.name or user.data.nickname or ""
 
                 url = make_url(path.EMPLOYEE_CHATTING_ROOMS,
                                host=settings["platform_host"],
