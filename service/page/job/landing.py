138db11c7ff18546c99f10ae46d2753c8149c3a1
huangxia <944421101@qq.com>
2019-11-12 14:29:19 +0800

    高级搜索

--- service/page/job/landing.py
+++ service/page/job/landing.py
@@ -247,6 +247,8 @@ class LandingPageService(PageService):
 
             ret.append(source)
 
+        self.logger.debug("@@@@@@@@@@-3 ret :{}".format(ret))
+
         return ret
 
     def sub_nested_dict(self, somedict, somekeys):
@@ -261,8 +263,8 @@ class LandingPageService(PageService):
                         for city in value:
                             citys.append(city.get(es_key.split(".")[1]))
                         ret.update({key: citys})
-                    elif key == "position_type":
-                        ret.update({key: 0 if value.get(es_key.split(".")[1]) is None else value.get(es_key.split(".")[1])}) # position_type是新增属性，老数据没有这个属性
+                    # elif key == "position_type":
+                    #     ret.update({key: 0 if value.get(es_key.split(".")[1]) is None else value.get(es_key.split(".")[1])}) # position_type是新增属性，老数据没有这个属性
                     else:
                         ret.update({key: value.get(es_key.split(".")[1])})
                 else:
@@ -273,10 +275,10 @@ class LandingPageService(PageService):
             es_key = self.get_by_value_dict(key, platform_const.LANDING)
             value = somedict.get(es_key.split(".")[0])
             if value:
-                if key == "position_type":
-                    ret = {key: 0 if value.get(es_key.split(".")[1]) is None else value.get(es_key.split(".")[1])}
-                else:
-                    ret = {key: value.get(es_key.split(".")[1])}
+                # if key == "position_type":
+                #     ret = {key: 0 if value.get(es_key.split(".")[1]) is None else value.get(es_key.split(".")[1])}
+                # else:
+                ret = {key: value.get(es_key.split(".")[1])}
             else:
                 ret = {key: ""}
         else:
