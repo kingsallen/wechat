0274c530a1fb7805e89d1469e2a96892892797a7
牛振亚 <niuzhenya@moseeker.com>
2019-11-18 17:23:29 +0800

    add async rabbitmq client

--- util/common/mq.py
+++ util/common/mq.py
@@ -1,13 +1,90 @@
 # coding=utf-8
 
 import pika
+from pika import TornadoConnection
 import pika_pool
 
 import json
-from globals import logger
+from globals import logger, env
 
 import conf.common as const
 from setting import settings
+
+
+class Publisher(object):
+    """
+    rabbitMQ客户端实现
+    部分方法（例如add_on_cancel_callback）只是记录了log，并没有做具体的实现，你通过重写这些方法，以实现各种功能
+    当前pika版本为0.10.0，参考`https://github.com/pika/pika/blob/0.10.0/docs/examples/tornado_consumer.rst`
+    """
+
+    def __init__(self, exchange, exchange_type, routing_key="", queue="", durable=True):
+        """ Create a new instance of the consumer class.
+
+        :param exchange: exchange 名
+        :param exchange_type: exchange 类型
+        :param routing_key: 默认路由键
+        :param queue: queue 名，允许为空
+        :param durable: 2: 消息持久化
+        """
+        self.exchange = exchange
+        self.exchange_type = exchange_type
+        self.routing_key = routing_key
+        self.queue = queue
+
+        # 默认消息持久化
+        self.durable = durable
+
+        self.connection = None
+        self.channel = None
+
+    def connect(self):
+        """ Connect to the broker """
+        logger.info("Connecting to {}".format(settings['rabbitmq_host']))
+
+        cred = pika.PlainCredentials(settings['rabbitmq_username'], settings['rabbitmq_password'])
+        param = pika.ConnectionParameters(
+            host=settings['rabbitmq_host'],
+            port=settings['rabbitmq_port'],
+            connection_attempts=settings['rabbitmq_connection_attempts'],
+            heartbeat_interval=settings['rabbitmq_heartbeat_interval'],
+            credentials=cred)
+        return TornadoConnection(param, on_open_callback=self.on_connected, on_close_callback=self.on_channel_closed)
+
+    def on_connected(self, connection):
+        logger.debug('PikaClient Publisher: connected to RabbitMQ')
+        self.connection = connection
+        self.connection.add_on_close_callback(self.on_closed)
+        self.connection.channel(self.on_channel_open)
+
+    def on_closed(self, connection, reply_code, reply_text):
+        logger.warning('PikaClient Publisher: rabbit connection closed, reopening in 1 seconds: (%s) %s', reply_code, reply_text)
+        self.connection.add_timeout(1, self.reconnect)
+
+    def reconnect(self):
+        self.connection = self.connect()
+
+    def on_channel_open(self, channel):
+        """override this method to bind exchange"""
+        # declare exchanges, which in turn, declare
+        # queues, and bind exchange to queues
+
+        logger.debug('PikaClient Publisher: Channel open, Declaring exchange {}'.format(const.REDPACKET_EXCHANGE))
+
+    def publish_message(self, message):
+        properties = pika.BasicProperties(app_id=const.APPID[env],
+                                          content_type='application/json',
+                                          delivery_mode=2 if self.durable else 1)
+
+        self.channel.basic_publish(self.exchange, self.routing_key,
+                                   json.dumps(message, ensure_ascii=False),
+                                   properties)
+
+    def on_channel_closed(self, channel, reply_code, reply_text):
+        logger.warning('Publisher Channel closed, reopening in 1 seconds: (%s) %s',
+                       reply_code, reply_text)
+        self.channel.close()
+
 
 
 class MQPublisher(object):
