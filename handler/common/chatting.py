cb6f700810152e6d6f1f12556df0fb0c81e420f5
jack frame weng <wjf2255@gmail.com>
2019-11-22 14:33:56 +0800

    增加get和post的判断

--- handler/common/chatting.py
+++ handler/common/chatting.py
@@ -78,6 +78,16 @@ class EmployeeChattingHandler(BaseHandler):
         if self.user_id == 0 and not self.current_user.employee:
             self.user_id = self.current_user.sysuser.id or 0
 
+        self.logger.debug("EmployeeChattingHandler get params role:{}, employee_id:{}, user_id:{}"
+                          .format(self.role, self.employee_id, self.user_id))
+
+        if self.role == "employee" and not self.current_user.employee:
+            self._send_json(data={}, status_code=30500, message=CHATTING_EMPLOYEE_RESIGNATION_TIPS, http_code=200)
+            return
+        if self.role == "user" and self.current_user.employee:
+            self._send_json(data={}, status_code=305072, message=CHATTING_EMPLOYEE_EMPLOYEE_TIPS, http_code=200)
+            return
+
         try:
             # 重置 event，准确描述
             if method and isinstance(method, str):
@@ -124,6 +134,16 @@ class EmployeeChattingHandler(BaseHandler):
 
         if self.user_id == 0 and not self.current_user.employee:
             self.user_id = self.current_user.sysuser.id or 0
+
+        self.logger.debug("EmployeeChattingHandler get params role:{}, employee_id:{}, user_id:{}"
+                          .format(self.role, self.employee_id, self.user_id))
+
+        if self.role == "employee" and not self.current_user.employee:
+            self._send_json(data={}, status_code=30500, message=CHATTING_EMPLOYEE_RESIGNATION_TIPS, http_code=200)
+            return
+        if self.role == "user" and self.current_user.employee:
+            self._send_json(data={}, status_code=305072, message=CHATTING_EMPLOYEE_EMPLOYEE_TIPS, http_code=200)
+            return
 
         try:
             # 重置 event，准确描述
