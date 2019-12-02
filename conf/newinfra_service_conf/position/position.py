7d7277d48cf5b30f5bdaa43a40ade87d74603771
牛振亚 <niuzhenya@moseeker.com>
2019-12-02 14:28:52 +0800

    修改url

--- conf/newinfra_service_conf/position/position.py
+++ conf/newinfra_service_conf/position/position.py
@@ -4,8 +4,8 @@ POSITION_LIST = "/v4/wx/list"
 POSITION_LIST_BY_PIDS = "/v4/list"
 POSITION_TEMPLATE_BY_PIDS = "/v4/position/referral/template/fields"
 POSITION_LIST_GET_DISTANCE = "/v4/position/distance"
-POSITION_ES_BY_ID = "/v4/position/wx"
-POSITION_LIST_ES = "/v4/position/wx/list"
+POSITION_ES_BY_ID = "/v4/search/wx/position"
+POSITION_LIST_ES = "/v4/search/wx/position/list"
 
 
 
